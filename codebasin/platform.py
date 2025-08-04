2f03bfbd0ccb0551a0d7668ecf02b7f6adfb864a
John Pennycook <john.pennycook@intel.com>
2025-08-04 16:51:18 +0100

    Add missing type hints to codebasin/platform.py
    Adding the correct type hints here highlighted that there are two functions
    with surprising behavior; I've added FIXME and TODO comments to ensure we do
    not lose track of these.
    
    Signed-off-by: John Pennycook <john.pennycook@intel.com>

--- codebasin/platform.py
+++ codebasin/platform.py
@@ -7,6 +7,8 @@ options for a specific platform.
 
 import os
 
+from codebasin.preprocessor import Macro, MacroFunction
+
 
 class Platform:
     """
@@ -14,29 +16,29 @@ class Platform:
     Contains a list of definitions, and include paths.
     """
 
-    def __init__(self, name, _root_dir):
-        self._definitions = {}
-        self._skip_includes = []
-        self._include_paths = []
+    def __init__(self, name: str, _root_dir: str) -> None:
+        self._definitions: dict[str, Macro | MacroFunction] = {}
+        self._skip_includes: list[str] = []
+        self._include_paths: list[str] = []
         self._root_dir = _root_dir
         self.name = name
-        self.found_incl = {}
+        self.found_incl: dict[str, str | None] = {}
 
-    def add_include_path(self, path):
+    def add_include_path(self, path: str) -> None:
         """
         Insert a new path into the list of include paths for this
         platform.
         """
         self._include_paths.append(path)
 
-    def undefine(self, identifier):
+    def undefine(self, identifier: str) -> None:
         """
         Undefine a macro for this platform, if it's defined.
         """
         if identifier in self._definitions:
             del self._definitions[identifier]
 
-    def define(self, identifier, macro):
+    def define(self, identifier: str, macro: Macro | MacroFunction) -> None:
         """
         Define a new macro for this platform, only if it's not already
         defined.
@@ -44,7 +46,7 @@ class Platform:
         if identifier not in self._definitions:
             self._definitions[identifier] = macro
 
-    def add_include_to_skip(self, fn):
+    def add_include_to_skip(self, fn: str) -> None:
         """
         Define a new macro for this platform, only if it's not already
         defined.
@@ -52,23 +54,24 @@ class Platform:
         if fn not in self._skip_includes:
             self._skip_includes.append(fn)
 
-    def process_include(self, fn):
+    def process_include(self, fn: str) -> bool:
         """
         Return a boolean stating if this include file should be
         processed or skipped.
         """
         return fn not in self._skip_includes
 
-    def is_defined(self, identifier):
+    # FIXME: This should return a bool, but the usage relies on a str.
+    def is_defined(self, identifier: str) -> str:
         """
-        Return a boolean stating if the macro named by 'identifier' is
+        Return a string representing whether the macro named by 'identifier' is
         defined.
         """
         if identifier in self._definitions:
             return "1"
         return "0"
 
-    def get_macro(self, identifier):
+    def get_macro(self, identifier: str) -> Macro | MacroFunction | None:
         """
         Return either a macro definition (if it's defined), or None.
         """
@@ -76,7 +79,12 @@ class Platform:
             return self._definitions[identifier]
         return None
 
-    def find_include_file(self, filename, this_path, is_system_include=False):
+    def find_include_file(
+        self,
+        filename: str,
+        this_path: str,
+        is_system_include: bool = False,
+    ) -> str | None:
         """
         Determine and return the full path to an include file, named
         'filename' using the include paths for this platform.
@@ -103,6 +111,7 @@ class Platform:
                 self.found_incl[filename] = include_file
                 return include_file
 
-        if include_file is None:
-            self.found_incl[filename] = None
-            return None
+        # TODO: Check this optimization is always valid.
+        assert include_file is None
+        self.found_incl[filename] = None
+        return None
